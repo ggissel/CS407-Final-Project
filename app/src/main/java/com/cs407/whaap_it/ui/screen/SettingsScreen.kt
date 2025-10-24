e9cb9cccc4ab292fd0984d188ce4d4f542bc71a7
xyap2 <xyap2@wisc.edu>
2025-10-24 17:18:54 -0500

    Added screen package in ui package, added new SettingsScreen.kt file in screen package w/ TODOs

---
+++ app/src/main/java/com/cs407/whaap_it/ui/screen/SettingsScreen.kt
@@ -0,0 +1,15 @@
+package com.cs407.whaap_it.ui.screen
+
+import androidx.compose.runtime.Composable
+
+/**
+ * The settings page is where the app settings can be configured
+ */
+@Composable
+fun SettingsScreen( // TODO: add navigation function back to home as param
+
+) {
+    // TODO: Title Header (Settings)
+
+    // TODO: Create a Card to hold all the toggle buttons, etc.
+}
