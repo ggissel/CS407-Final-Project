9606327073c20a09bc25c7872c943d0cb68caabe
xyap2 <xyap2@wisc.edu>
2025-10-24 17:48:28 -0500

    Modified header title in settings to include blue background and white text on the top of the page (max width)

--- app/src/main/java/com/cs407/whaap_it/ui/screen/SettingsScreen.kt
+++ app/src/main/java/com/cs407/whaap_it/ui/screen/SettingsScreen.kt
@@ -1,5 +1,6 @@
 package com.cs407.whaap_it.ui.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,8 +10,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -34,7 +37,12 @@ fun SettingsScreen( // TODO: add navigation function back to home as param
         Text( // TODO: maintain styling consistency w/ rest of the app
             text = stringResource(id = R.string.settings_page_title),
             style = MaterialTheme.typography.headlineMedium,
-            modifier = Modifier.padding(top = 24.dp, bottom = 24.dp)
+            color = Color.White,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color.Blue)
+                .padding(24.dp)
         )
 
         // TODO: Create an ElevatedCard to hold all the toggle buttons, etc.
