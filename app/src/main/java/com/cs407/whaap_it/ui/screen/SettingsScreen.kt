3462acca209b8fcd38018590649631db5d2d5053
xyap2 <xyap2@wisc.edu>
2025-10-24 20:37:49 -0500

    Added SettingsTab which makes settings vertically scrollable, added SettingsToggleRow to display each settings option, added GameSettings data class to store toggle state of each settings option

--- app/src/main/java/com/cs407/whaap_it/ui/screen/SettingsScreen.kt
+++ app/src/main/java/com/cs407/whaap_it/ui/screen/SettingsScreen.kt
@@ -1,18 +1,28 @@
 package com.cs407.whaap_it.ui.screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.PrimaryTabRow
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -20,6 +30,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.cs407.whaap_it.R
@@ -57,6 +68,9 @@ fun SettingsScreen( // TODO: add navigation function back to home as param
 
 }
 
+/**
+ * Use this to preview the SettingsScreen()
+ */
 @Preview(showBackground = true)
 @Composable
 fun SettingsScreenPreview() {
@@ -65,14 +79,17 @@ fun SettingsScreenPreview() {
     }
 }
 
+/**
+ * This is the visual Card container that houses the settings options
+ */
 @Composable
 fun Cardfolio() {
-    Box(
+    Box( // Container
         contentAlignment = Alignment.Center,
         modifier = Modifier
             .fillMaxSize()
     ){
-        ElevatedCard(
+        ElevatedCard( // Card
             modifier = Modifier
                 .fillMaxSize()
                 .padding(24.dp),
@@ -82,7 +99,81 @@ fun Cardfolio() {
                 containerColor = MaterialTheme.colorScheme.primaryContainer
             )
         ){
-
+            // TODO: add Tabs to navigate to different settings type
+            SettingsTab()
         }
     }
 }
+
+/**
+ * This will display all the settings options related to gameplay
+ * such as toggles for tilt, microphone, etc.
+ */
+@Composable
+fun SettingsTab() {
+    var settings by remember { mutableStateOf(GameSettings()) }
+
+    LazyColumn( // Vertical scroll container
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        item {
+            SettingsToggleRow(
+                title = "Tilt Feature",
+                isChecked = settings.enableTilt,
+                onCheckedChange = { isEnabled ->
+                    settings = settings.copy(enableTilt = isEnabled)
+                }
+            )
+        }
+        item {
+            SettingsToggleRow(
+                title = "Microphone",
+                isChecked = settings.enableTilt,
+                onCheckedChange = { isEnabled ->
+                    settings = settings.copy(enableTilt = isEnabled)
+                }
+            )
+        }
+    }
+}
+
+/**
+ * Displays each setting option with a toggle button
+ */
+@Composable
+fun SettingsToggleRow(
+    title: String,
+    isChecked: Boolean,
+    onCheckedChange: (Boolean) -> Unit
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(36.dp)
+    ) {
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.primary,
+        )
+        Switch(
+            checked = isChecked,
+            onCheckedChange = onCheckedChange,
+            colors = SwitchDefaults.colors() // TODO: change the toggle color
+        )
+    }
+}
+
+/**
+ * Holds the toggle state of each game settings option
+ */
+data class GameSettings(
+    val enableTilt: Boolean = false,
+    val useMicrophone: Boolean = false,
+    val hapticFeedback: Boolean = true
+)
