4db417419c51145cf4f2292172896902be48a9ae
xyap2 <xyap2@wisc.edu>
2025-11-02 17:34:58 -0600

    Added a new LoginSignupScreen.kt where users can login/signup with an account. Added some strings in strings.xml needed for Login/Signup buttons.

---
+++ app/src/main/java/com/cs407/whaap_it/ui/screen/LoginSignupScreen.kt
@@ -0,0 +1,54 @@
+package com.cs407.whaap_it.ui.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.cs407.whaap_it.R
+import com.cs407.whaap_it.ui.theme.WhaapitTheme
+
+/**
+ * The login/signup page where users can either create a new account or login to an existing one
+ */
+@Composable
+fun LoginSignupScreen(
+    modifier: Modifier = Modifier
+) {
+    Scaffold(modifier) {
+        innerPadding ->
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+            Button(
+                onClick = {}
+            ) {
+                Text(stringResource(R.string.login_button))
+            }
+            Button(
+                onClick = {}
+            ) {
+                Text(stringResource(R.string.signup_button))
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginSignupScreenPreview() {
+    WhaapitTheme {
+        LoginSignupScreen()
+    }
+}
