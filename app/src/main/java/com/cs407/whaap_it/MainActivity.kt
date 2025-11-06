3f51ea3889b06f824143d48a03ce46e69cdcd1c9
jdrosales4 <jdrosales@wisc.edu>
2025-11-06 17:35:05 -0600

    Added logo to home screen

--- app/src/main/java/com/cs407/whaap_it/MainActivity.kt
+++ app/src/main/java/com/cs407/whaap_it/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.List
@@ -18,6 +19,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -177,14 +181,15 @@ fun StartScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = "WHAAP IT!",
-            fontSize = 48.sp,
-            fontWeight = FontWeight.Bold,
-            color = MaterialTheme.colorScheme.primary
-        )
-
-        Spacer(modifier = Modifier.height(64.dp))
+        Box {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "whaap it logo",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(400.dp)
+            )
+        }
 
         Button(
             onClick = { onNavigateToGameScreen() },
