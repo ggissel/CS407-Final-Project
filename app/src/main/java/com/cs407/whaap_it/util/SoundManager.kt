90e20b433bd925901205a3ecfdf8d64563a12dd1
xyap2 <xyap2@wisc.edu>
2025-11-06 13:15:52 -0600

    Modified SoundManager to use SoundPool instead of MediaPlayer for short-form audiotracks (less lag), Added toggle button on & off sound in res/raw

--- app/src/main/java/com/cs407/whaap_it/util/SoundManager.kt
+++ app/src/main/java/com/cs407/whaap_it/util/SoundManager.kt
@@ -1,32 +1,46 @@
 package com.cs407.whaap_it.util
 
 import android.content.Context
-import android.media.MediaPlayer
+import android.media.AudioAttributes
+import android.media.SoundPool
 import com.cs407.whaap_it.R
 
 /**
- * Helper singleton method to manage audio/sound functions such as button clicks
+ * Helper companion object used to manage the in-app sounds, such as Button Clicks
  */
 object SoundManager {
-    private var buttonClickSound: MediaPlayer? = null
+    private lateinit var soundPool: SoundPool
+    private var buttonClickSound: Int = 0
+
+    private var toggleOnSound: Int = 0
+
+    private var toggleOffSound: Int = 0
 
     fun init(context: Context) {
-        if (buttonClickSound == null) {
-            buttonClickSound = MediaPlayer.create(context, R.raw.ui_button_click_1)
-        }
+        val audioAttributes = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_GAME)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .build()
+
+        soundPool = SoundPool.Builder()
+            .setMaxStreams(5)
+            .setAudioAttributes(audioAttributes)
+            .build()
+
+        // Load short sounds
+        buttonClickSound = soundPool.load(context, R.raw.ui_button_click_1, 1)
+
     }
 
     fun playButtonClick() {
-        buttonClickSound?.let {
-            if (it.isPlaying) {
-                it.seekTo(0)
-            }
-            it.start()
-        }
+        soundPool.play(buttonClickSound, 1f, 1f, 1, 0, 1f)
+    }
+
+    fun playToggleOnSound() {
+
     }
 
     fun release() {
-        buttonClickSound?.release()
-        buttonClickSound = null
+        soundPool.release()
     }
 }
