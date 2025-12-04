4581fa65ba7f6969ab29ba047039794feccdf0c0
xyap2 <xyap2@wisc.edu>
2025-12-04 02:05:55 -0600

    Added Settings Functionality and DataStore. Ability to now store app settings such as app & music volume.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,5 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
     implementation("com.google.firebase:firebase-auth")
+    implementation("androidx.datastore:datastore-preferences:1.2.0")
 }
--- app/src/main/java/com/cs407/whaap_it/ui/screen/SettingsScreen.kt
+++ app/src/main/java/com/cs407/whaap_it/ui/screen/SettingsScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
@@ -34,6 +35,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -44,6 +46,7 @@ import com.cs407.whaap_it.R
 import com.cs407.whaap_it.ui.theme.WhaapitTheme
 import com.cs407.whaap_it.ui.viewModels.SettingsViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.cs407.whaap_it.ui.viewModels.SettingsViewModelFactory
 import com.cs407.whaap_it.util.SoundManager
 
 /**
@@ -51,9 +54,12 @@ import com.cs407.whaap_it.util.SoundManager
  */
 @Composable
 fun SettingsScreen(
-    viewModel: SettingsViewModel = viewModel(),
     onNavigateToHome: () -> Unit = {}
 ) {
+    val context = LocalContext.current
+    val viewModel: SettingsViewModel = viewModel(
+        factory = SettingsViewModelFactory(context)
+    )
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
@@ -96,7 +102,7 @@ fun SettingsScreen(
         }
 
 
-        Cardfolio(viewModel)
+        Cardfolio(viewModel = viewModel)
     }
 
 }
@@ -117,9 +123,9 @@ fun SettingsScreenPreview() {
  */
 @Composable
 fun Cardfolio(
-    viewModel: SettingsViewModel = viewModel()
+    viewModel: SettingsViewModel
 ) {
-    var settings by viewModel.settingsState
+    val settings by viewModel.settingsState.collectAsState()
     val gameplaySettings = listOf(
         SettingItem.ToggleItem(
             title = "Shake Feature",
